4b57f0ddfd220e1cfbbcebb3a00d158ff098f24c
sibirkoda <denis.a.sibirko@mail.ru>
2022-08-29 19:51:04 +0300

    started doing a seminar from 2022-08-27

---
+++ Lesson_2022-08-27/Program.cs
@@ -0,0 +1,12 @@
+﻿void Method1()
+{
+    Console.WriteLine("Hello, World!");
+}
+
+ Method1();
+
+// void Method2(string msg)
+// {
+//     Console.WriteLine(msg);
+// }
+// Console.WriteLine("Hello, World!");
